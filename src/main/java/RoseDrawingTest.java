dbffaaa123f5236bcebb39e05e13e589b7947030
Your Name <you@example.com>
2024-01-29 23:49:10 +0100

    4. commit

---
+++ src/main/java/RoseDrawingTest.java
@@ -0,0 +1,22 @@
+package main.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoseDrawingTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateUI() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getUI() {
+    }
+}
